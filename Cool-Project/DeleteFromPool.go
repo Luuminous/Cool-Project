7c84d58f854eb83b7bbc6c1510e4164099fbfb58
Luuminous <34859791+Luuminous@users.noreply.github.com>
2018-10-14 16:24:11 -0400

    Update DeleteFromPool.go

--- Cool-Project/DeleteFromPool.go
+++ Cool-Project/DeleteFromPool.go
@@ -8,7 +8,7 @@ package CoolProject
 /*
 This function is to delete the out cards from the pool cards.
 */
-func DeleteFormPool(pool []Card, out []Card) []Card {
+func DeleteFromPool(pool []Card, out []Card) []Card {
 	var ans []Card
 	for i := 0; i < len(pool); i++ {
 		check := true
