d05f662a25205d67176e5b2854c7c54f8e112a68
Luuminous <34859791+Luuminous@users.noreply.github.com>
2018-10-22 16:50:32 -0400

    Add files via upload

--- test.go
+++ test.go
@@ -3,15 +3,15 @@ package main
 import (
 	"CoolProject"
 	"math/rand"
+	"os"
+	"strconv"
 	"time"
 )
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	cardList := make([]CoolProject.Card, 2)
-	cardList[0].Num = 11
-	cardList[0].Color = 1
-	cardList[1].Num = 10
-	cardList[1].Color = 0
-	CoolProject.CalculateProbability(100000)
+	numPlayers, _ := strconv.Atoi(os.Args[1])
+	initialChips, _ := strconv.Atoi(os.Args[2])
+	numTurns, _ := strconv.Atoi(os.Args[3])
+	CoolProject.PlayGame(numPlayers, initialChips, numTurns)
 }
