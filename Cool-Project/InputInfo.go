d8eca15fb0cd0a2b50ffe014f099bb9156040a5c
yxfwfqyd <37560330+yxfwfqyd@users.noreply.github.com>
2018-10-31 18:46:34 -0400

    Update InputInfo.go

--- Cool-Project/InputInfo.go
+++ Cool-Project/InputInfo.go
@@ -31,7 +31,7 @@ func InputInfo(actionList []string, max, bet, currentbet int) string {
 		aMoney := string(money)
 		intMon, err := strconv.Atoi(aMoney)
 
-		for (intMon >= max) ||(intMon < currentbet-bet)|| err != nil {
+		for (intMon >= max) || (intMon < currentbet-bet) || err != nil {
 			reader := bufio.NewReader(os.Stdin)
 			fmt.Println("Invalid money number, please enter again: ")
 			money, _, _ = reader.ReadLine()
