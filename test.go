1f861be1a08f4208b22649f3e8cf352f14f2a771
Luuminous <34859791+Luuminous@users.noreply.github.com>
2018-10-31 15:05:06 -0400

    Add files via upload

--- test.go
+++ test.go
@@ -10,8 +10,14 @@ import (
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
+
 	numPlayers, _ := strconv.Atoi(os.Args[1])
 	initialChips, _ := strconv.Atoi(os.Args[2])
 	numTurns, _ := strconv.Atoi(os.Args[3])
+	/*
+	numPlayers := 4
+	initialChips := 1000
+	numTurns := 5
+	*/
 	CoolProject.PlayGame(numPlayers, initialChips, numTurns)
 }
